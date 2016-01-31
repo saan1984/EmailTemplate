d5c70d47a226d29b96f5831c0f4999d716d1574a
spatel10 <sandeepkumar_patel@intuit.com>
2016-02-01 00:21:59 +0530

    changes

--- generator/app.js
+++ generator/app.js
@@ -5,12 +5,13 @@ app.controller("GenController", function($scope,$window,$mdSidenav){
 	$scope.makeCopy= function(){
 		clipboard.copy({
 			"text/html": $scope.result
-		})
+		});
 	}
 	$scope.generate = function(){
 		var stylePreview = "<head><style>"+document.getElementById("style").cloneNode(true).innerHTML+"</style></head>",
 			htmlPreview = "<body>"+document.getElementById("generatedPreview").cloneNode(true).innerHTML+"</body>";
 		$scope.result =   "<html>"+stylePreview+htmlPreview+"</html>";
+		$mdSidenav('right').toggle();
 	}
 
 	$scope.createNewTip = function() {

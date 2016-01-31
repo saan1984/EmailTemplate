5df1e3348f0a08a951c97cfd14e24668ce10eabb
spatel10 <sandeepkumar_patel@intuit.com>
2016-01-31 23:10:53 +0530

    changes

--- generator/app.js
+++ generator/app.js
@@ -1,19 +1,19 @@
 var app = angular.module("myGenApp",['ngAnimate',"ngMaterial",'imageupload']);
 
-app.controller("GenController", function($scope,$mdSidenav){
+app.controller("GenController", function($scope,$window,$mdSidenav){
 
-		$scope.makeCopy= function(){
-				var stylePreview = "<style>"+document.getElementById("style").cloneNode(true).innerHTML+"<style>",
-				htmlPreview = "<body>"+document.getElementById("generatedPreview").cloneNode(true).innerHTML+"</body>";
-			$scope.result =   htmlPreview+stylePreview;
-		}
-
-
-	$scope.openLeftMenu = function() {
+	$scope.makeCopy= function(){
 		clipboard.copy({
-			"text/plain": $scope.result,
 			"text/html": $scope.result
 		})
-		$mdSidenav('left').toggle();
+	}
+	$scope.generate = function(){
+		var stylePreview = "<head><style>"+document.getElementById("style").cloneNode(true).innerHTML+"</style></head>",
+			htmlPreview = "<body>"+document.getElementById("generatedPreview").cloneNode(true).innerHTML+"</body>";
+		$scope.result =   "<html>"+stylePreview+htmlPreview+"</html>";
+	}
+
+	$scope.createNewTip = function() {
+		$window.location.reload();
 	};
 });
